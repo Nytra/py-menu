a959545fc5ede88a674508353f63e06128b73ef4
Nytra <samueltscott@gmail.com>
2016-10-14 01:10:41 +0100

    The program now displays the menu options correctly and allows for selection using the arrow keys

--- menu.py
+++ menu.py
@@ -1,11 +1,11 @@
 from colorama import init, Fore, Back, Style
-import msvcrt
+import msvcrt, shutil
 
 init(autoreset=True)
 
 class Menu:
-    def __init__(self, dimensions, title="Test Menu", fg=Fore.WHITE, bg=Back.BLUE):
-        self.X_MAX,self.Y_MAX = dimensions
+    def __init__(self, title="Main Menu", fg=Fore.WHITE, bg=Back.BLUE):
+        self.X_MAX,self.Y_MAX = shutil.get_terminal_size((80, 20)).columns, shutil.get_terminal_size((80, 20)).lines
         self.options = []
         self.bg = bg 
         self.fg = fg
@@ -13,10 +13,7 @@ class Menu:
         self.selected = 0 # first option
 
         self.option_width = self.X_MAX // 3
-        self.option_height = self.Y_MAX // 10
-        
-        self.option_zone_x = (self.X_MAX//2) - (self.option_width//2) # In the middle
-        self.option_zone_y = (self.Y_MAX//2) + (self.option_height//2) # In the middle but also pushed slightly towards the bottom
+        self.option_height = 1
 
     def add(self, text, target):
         self.options.append([text,target]) 
@@ -32,37 +29,44 @@ class Menu:
 
                 self.put([x,y], self.bg + " ")
 
+        
+
         for y in range(1, self.Y_MAX):
             for x in range(1, self.X_MAX):
 
                 if y == (self.Y_MAX // 10) and x == (self.X_MAX // 2) - (len(self.title) // 2):
                     self.put([x,y], self.bg + self.fg + self.title)
 
-                if y >= self.option_zone_y and y < self.Y_MAX - (self.Y_MAX - self.option_zone_y):
 
 
-                    n = len(self.options)
-                    for index, option in enumerate(self.options):
-                        text = self.options[index][0]
-                        ox = (self.X_MAX // 2) - (len(text) // 2)
 
-                        if x == ox and y == oy:
-                            self.put([x,y], self.bg + self.fg + text)
+        options_drawn = 0        
+        for index, option in enumerate(self.options):
+            text = self.options[index][0]
+                        
+            option_x = (self.X_MAX//2) - (len(text)//2) # In the middle
+            option_y = (self.Y_MAX//2) + options_drawn
+
+            box_x = (self.X_MAX // 3)
+            options_drawn += 1
+            for x in range(box_x, self.X_MAX - box_x):
+                self.put([x,option_y], Back.WHITE + " ")
+            self.put([box_x,option_y], Back.WHITE + Fore.BLACK + str(index+1) + ")")
+            self.put([option_x,option_y], Back.WHITE + Fore.BLACK + text)
 
     def move_down(self):
-        if self.selected >= len(self.options-1):
+        if self.selected >= len(self.options) - 1:
             self.selected = 0
         else:
             self.selected += 1
 
     def move_up(self):
         if self.selected <= 0:
-            self.selected = len(self.options-1)
+            self.selected = len(self.options) - 1
         else:
             self.selected -= 1
 
     def start(self):
-        index = 0
         self.draw()
         while True:
             if msvcrt.kbhit():
@@ -75,12 +79,26 @@ class Menu:
                     elif key == 72:
                         self.move_up()
                         self.draw()
+                elif key == 13: # enter key
+                    self.put([1,1], "")
+                    f = self.options[self.selected][1]
+                    f()
+                    self.draw()
 
 if __name__ == "__main__":
 
     def test_function():
         print("Hello, world!")
+        input("Press enter to continue.")
     
-    m = Menu([80,20])
-    m.add("Test", target=test_function)
-    m.start()
+    #title = input("Menu title: ")
+    
+
+    m = Menu()
+    m.add("Hello World", target=test_function)
+    m.add("Exit", target= lambda: quit() )
+    try:
+        m.start()
+    except Exception as e:
+        print(e)
+        input()
